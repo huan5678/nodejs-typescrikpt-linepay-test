f024e31f7687373fca372336557b49e15c2214ed
黃士桓 <huan5678@gmail.com>
2023-04-17 17:44:49 +0800

    :construction: 增加linepay前端測試頁面

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,40 @@
+import path from 'path';
 import bodyParser from 'body-parser';
-import express, {Request, Response, NextFunction} from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import QRCode from 'qrcode';
 import {Configuration, OpenAIApi} from 'openai';
 import dotenv from 'dotenv';
 import dayjs from 'dayjs';
 import { v4 as uuidv4 } from 'uuid';
-import {createLinePayClient} from 'line-pay-merchant';
+import { createLinePayClient } from 'line-pay-merchant';
 
 const app = express();
 const port = 3000;
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, 'pages')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'pages'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.json());
 dotenv.config();
+
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+
+
+app.get('/', (req: Request, res: Response) =>
+{
+  const data = '';
+
+  res.render('index', {data});
+});
 
 app.post('/', async (req, res, next) => {
   try {
