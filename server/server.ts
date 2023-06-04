519968f88548b37c2b0dd5f8af2ab6ce34dca3d3
黃士桓 <huan5678@gmail.com>
2023-06-05 03:55:24 +0800

    wip

--- server/server.ts
+++ server/server.ts
@@ -8,6 +8,8 @@ import dotenv from 'dotenv';
 import dayjs from 'dayjs';
 import { v4 as uuidv4 } from 'uuid';
 import { createLinePayClient } from 'line-pay-merchant';
+import axios from 'axios';
+import NewebpayClient from 'newebpay-mpg-sdk';
 
 const app = express();
 const port = 3000;
@@ -17,20 +19,17 @@ app.use(express.static(path.join(__dirname, 'pages')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'pages'));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use(express.json());
 dotenv.config();
-
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
-app.get('/', (req: Request, res: Response) =>
-{
+app.get('/', (req: Request, res: Response) => {
   const data = '';
 
   res.render('index', {data});
@@ -72,9 +71,19 @@ declare global {
       LINE_PAY_CHANNEL_SECRET: string;
       LINE_PAY_ENV: 'development' | 'production';
       NODE_ENV: 'dev' | 'production';
+      NEWEBPAY_MERCHANT_ID: string;
+      NEWEBPAY_HASH_KEY: string;
+      NEWEBPAY_HASH_IV: string;
     }
   }
 }
+
+const newebPayClient = new NewebpayClient({
+  merchantId: process.env.NEWEBPAY_MERCHANT_ID,
+  hashKey: process.env.NEWEBPAY_HASH_KEY,
+  hashIV: process.env.NEWEBPAY_HASH_IV,
+  env: 'sandbox', // 'sandbox' | 'production'
+});
 
 const linePayConfig = {
   channelId: process.env.LINE_PAY_CHANNEL_ID,
@@ -127,7 +136,7 @@ app.post('/line-pay/request', async (req: Request, res: Response, next: NextFunc
       },
     ],
   };
-  orders[ orderId ] = order;
+  orders[orderId] = order;
   try {
     const response = await linePay.request.send({
       body: {
@@ -136,7 +145,7 @@ app.post('/line-pay/request', async (req: Request, res: Response, next: NextFunc
           confirmUrl,
           cancelUrl,
         },
-      }
+      },
     });
     res.send(response);
   } catch (error) {
@@ -145,11 +154,10 @@ app.post('/line-pay/request', async (req: Request, res: Response, next: NextFunc
   }
 });
 
-app.get('/line-pay/confirm', async (req: Request, res: Response, next: NextFunction) =>
-{
+app.get('/line-pay/confirm', async (req: Request, res: Response, next: NextFunction) => {
   try {
     const {transactionId, orderId} = req.query as {transactionId: string; orderId: string};
-    const { amount, currency } = orders[orderId];
+    const {amount, currency} = orders[orderId];
     const response = await linePay.confirm.send({
       transactionId,
       body: {
@@ -165,13 +173,12 @@ app.get('/line-pay/confirm', async (req: Request, res: Response, next: NextFunct
   }
 });
 
-app.post('/line-pay/check-payment', async (req: Request, res: Response, next: NextFunction) =>
-{
+app.post('/line-pay/check-payment', async (req: Request, res: Response, next: NextFunction) => {
   try {
-  const response = await linePay.checkPaymentStatus.send({
-    transactionId: req.body.transactionId,
-    params: {},
-  });
+    const response = await linePay.checkPaymentStatus.send({
+      transactionId: req.body.transactionId,
+      params: {},
+    });
     res.send(response);
   } catch (error) {
     console.error(error);
@@ -179,21 +186,69 @@ app.post('/line-pay/check-payment', async (req: Request, res: Response, next: Ne
   }
 });
 
-app.post('/line-pay/payment-details', async (req: Request, res: Response, next: NextFunction) =>
-{
+app.post('/line-pay/payment-details', async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { transactionId } = req.body;
-  const response = await linePay.paymentDetails.send({
-    params: {
-      transactionId,
-    },
-  });
+    const {transactionId} = req.body;
+    const response = await linePay.paymentDetails.send({
+      params: {
+        transactionId,
+      },
+    });
     res.send(response);
   } catch (error) {
     console.error(error);
     next(error);
   }
-})
+});
+
+app.post('/neweb-pay/payment', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const response = newebPayClient.getPaymentFormHTML({
+      MerchantOrderNo: Date.now().toString(), // 訂單編號 必填
+      Amt: 1000, // 訂單金額 必填
+      ItemDesc: '商品1,商品2', // 商品資訊 必填 以逗號 (,) 分格, 最多 50 字元
+      // RespondType: "JSON", // 回傳格式 非必填 'JSON' | 'String'
+      TradeLimit: 900, // 交易限制秒數 非必填 60 - 900 秒
+      // ExpireDate: "20200729", // 繳費有效期限 非必填 格式：YYYYMMDD
+      ReturnURL: '', // 支付完成返回商店網址 非必填
+      NotifyURL: '', // 支付通知網址 非必填
+      CustomerURL: '', // 商店取號網址 非必填
+      ClientBackURL: '', // 返回商店網址 非必填
+      Email: 'test@example.com', // 付款人電子信箱 非必填
+      EmailModify: 1, // 付款人電子信箱是否開放修改 非必填 1 | 0
+      OrderComment: '訂單訊息', // 商店備註 非必填 最多 300 字元
+      Version: '2.0', // 程式版本 非必填
+      LangType: 'zh-tw', // 語系 非必填 'zh-tw' | 'en' | 'jp'
+      LoginType: 0, // 是否登入藍新金流會員 非必填 1 | 0
+      CREDIT: 1, // 信用卡一次付清啟用 非必填 1 | 0
+      ANDROIDPAY: 0, // Google Pay 啟用 非必填 1 | 0
+      SAMSUNGPAY: 0, // Samsung Pay 啟用 非必填 1 | 0
+      LINEPAY: 0, // Line Pay 啟用 非必填 1 | 0
+      ImageUrl: '', // Line Pay 產品圖檔連結網址 非必填
+      InstFlag: '3,6', // 信用卡分期付款啟用 非必填 '3' | '6' | '12' | '18' | '24' | '30'
+      CreditRed: 0, // 信用卡紅利啟用 非必填 1 | 0
+      UNIONPAY: 0, // 信用卡銀聯卡啟用 非必填 1 | 0
+      WEBATM: 0, // WEBATM 啟用 非必填 1 | 0
+      VACC: 1, // ATM 轉帳啟用 非必填 1 | 0
+      BankType: '', // 金融機構 非必填
+      BARCODE: 0, // 超商條碼繳費啟用 非必填 1 | 0
+      ESUNWALLET: 0, // 玉山 Wallet 啟用 非必填 1 | 0
+      TAIWANPAY: 0, // 台灣 Pay 啟用 非必填 1 | 0
+      CVSCOM: 0, // 物流啟用 非必填 1 | 0
+      EZPAY: 0, // 簡單付電子錢包啟用 非必填 1 | 0
+      EZPWECHAT: 0, // 簡單付微信支付啟用 非必填 1 | 0
+      EZPALIPAY: 0, // 簡單付支付寶啟用 非必填 1 | 0
+      // LgsType: "", // 物流型態 非必填 'B2C' | 'C2C'
+    });
+    console.log(response);
+    res.send(response);
+  } catch (error) {
+    console.error('getError', error);
+    next(error);
+  }
+});
+
+
 
 interface QRCodeData {
   id: string;
